6137a9b1341a7f336ef0edcd711f6c67e687045b
Andrea Ruzzenenti <ext-andrea.ruzzenenti@globant.com>
2026-02-10 12:15:05 +0100

    script for images implemented

---
+++ imgChange.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+
+cartella_target = r"C:\Users\ext-andrea.ruzzenent\Documents\ruzzeglobant.github.io\3326a99fc5ccd74646b7a069d57cb192_20260126"
+
+estensioni_immagini = (".png", ".jpg", ".jpeg", ".bmp", ".tiff", ".webp")
+
+for root, dirs, files in os.walk(cartella_target):
+    for file in files:
+        if file.lower().endswith(estensioni_immagini):
+            percorso = os.path.join(root, file)
+
+            try:
+                img = Image.open(percorso).convert("RGBA")
+                w, h = img.size
+
+                # Crea immagine completamente trasparente
+                img_trasparente = Image.new("RGBA", (w, h), (0, 0, 0, 0))
+
+                nome, ext = os.path.splitext(file)
+
+                # Se JPG/JPEG → salva come PNG
+                if ext.lower() in (".jpg", ".jpeg", ".bmp"):
+                    nuovo_percorso = os.path.join(root, nome + ".png")
+                    img_trasparente.save(nuovo_percorso, "PNG")
+                    os.remove(percorso)
+                else:
+                    img_trasparente.save(percorso)
+
+                print(f"Resa trasparente: {percorso}")
+
+            except Exception as e:
+                print(f"Errore con {percorso}: {e}")
